c0c4f50793612941b49bf4d669037ef8aaaf7983
Maxim Lakin <maximlakin@gmail.com>
2016-09-01 16:16:34 -0700

    stock data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -9,6 +10,12 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/graph', methods = ['POST'])
+def graph():
+  url = "https://www.quandl.com/api/v3/datasets/WIKI/"+request.form['ticker-symbol']+".json?api_key=vD5s74xXzwWxuqKYFqYR"
+  r = requests.get(url)
+  return render_template('graph.html', result = r.json())
 
 if __name__ == '__main__':
   app.run(port=33507)
