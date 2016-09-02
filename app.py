f3bf03465aea45413b34eda23f395d5ca753bd57
Maxim Lakin <maximlakin@gmail.com>
2016-09-01 18:50:04 -0700

    plot data

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import requests
+import numpy as np
+from bokeh.plotting import figure
+from bokeh.embed import components
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -13,9 +16,27 @@ def index():
 
 @app.route('/graph', methods = ['POST'])
 def graph():
-  url = "https://www.quandl.com/api/v3/datasets/WIKI/"+request.form['ticker-symbol']+".json?api_key=vD5s74xXzwWxuqKYFqYR"
-  r = requests.get(url)
-  return render_template('graph.html', result = r.json())
+  symbol = request.form['ticker-symbol']
+  api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=vD5s74xXzwWxuqKYFqYR' % symbol
+  raw_data = requests.get(api_url)
+  prices = []
+  dates = []
+  recent = raw_data.json()['data'][0:30]
+  recent.reverse()
+  for entry in recent:
+      prices.append(entry[4])
+      dates.append(entry[0])
+  plot = figure(
+                title='Data from Quandle WIKI set',
+                x_axis_label='date',
+                x_axis_type='datetime')
+
+  dates = np.array(dates, dtype=np.datetime64)
+  prices = np.array(prices)
+  plot.line(dates,prices)
+  script, div = components(plot)
+
+  return render_template('graph.html', script=script, div=div, symbol=symbol)
 
 if __name__ == '__main__':
   app.run(port=33507)
